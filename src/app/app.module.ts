d07ed9be4a36a23acb55ac1ba70ca4be349aef65
histlog <safejp1@gmail.com>
2022-01-12 00:02:02 +0530

    Updated new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PrimitiveControlsComponent } from './primitive-controls/primitive-controls.component';
+import { DemoComponent } from './demo/demo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrimitiveControlsComponent
+    PrimitiveControlsComponent,
+    DemoComponent
   ],
   imports: [
     BrowserModule
