3a091b871340dc9c6606cd1ca4203d47c67d7b47
histlog <safejp1@gmail.com>
2022-01-12 13:38:00 +0530

    Updated renderer in demo

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 @Component({
   selector: 'app-demo',
@@ -10,6 +12,23 @@ export class DemoComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.main();
   }
 
+  main() {
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(
+      45,
+      window.innerWidth/window.innerHeight,
+      1,
+      1000
+    );
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.getElementById('webgl').appendChild(renderer.domElement);
+    renderer.render(
+      scene,
+      camera
+    )
+  }
 }
