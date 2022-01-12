993746f668f40f81a0620c508e22635e38388f20
histlog <safejp1@gmail.com>
2022-01-12 23:47:31 +0530

    Updated camera position

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -17,12 +17,23 @@ export class DemoComponent implements OnInit {
 
   main() {
     var scene = new THREE.Scene();
+    
+    var box = this.getBox(1, 1, 1);
+    scene.add(box);
+
     var camera = new THREE.PerspectiveCamera(
       45,
       window.innerWidth/window.innerHeight,
       1,
       1000
     );
+
+    camera.position.z = 5;
+    camera.position.x = 1;
+    camera.position.y = 2;
+
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.getElementById('webgl').appendChild(renderer.domElement);
@@ -30,5 +41,14 @@ export class DemoComponent implements OnInit {
       scene,
       camera
     )
+  }
+
+  getBox(w, h, d) {
+    var geometry = new THREE.BoxGeometry(w, h, d);
+    var material = new THREE.MeshBasicMaterial({ 
+      color: 0x00f0
+    });
+    var mesh = new THREE.Mesh(geometry, material);
+    return mesh;
   }
 }
