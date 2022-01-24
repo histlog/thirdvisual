4f09e4f114b8873edc3df2391554d4b78940edb5
histlog <safejp1@gmail.com>
2022-01-25 00:22:52 +0530

    Updated GUI library

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import * as dat from 'dat.gui';
 
 @Component({
   selector: 'app-demo',
@@ -20,15 +21,30 @@ export class DemoComponent implements OnInit {
 
   main() {
     var scene = new THREE.Scene();
+    let enableFog = false;
+
+    let gui = new dat.GUI();
+
+    if(enableFog) {
+      scene.fog = new THREE.FogExp2(0xffffff, 0.2);
+    }
     
     let box = this.getBox(1, 1, 1);
     box.position.y = box.geometry.parameters.height/2;
 
-    let plane = this.getPlane(4);
+    let plane = this.getPlane(20);
     plane.rotation.x = Math.PI/2;
+    plane.name = 'plane-1';
+
+    let pointLight = this.getPointLight('#ffffff', 1);
+    pointLight.position.y = 2;
+
+    let sphere = this.getSphere(0.05);
+    pointLight.add(sphere);
 
     scene.add(box);
     scene.add(plane);
+    scene.add(pointLight);
 
     let camera = new THREE.PerspectiveCamera(
       45,
@@ -45,6 +61,7 @@ export class DemoComponent implements OnInit {
 
     let renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor('#d1d1d1');
     document.getElementById('webgl').appendChild(renderer.domElement);
     this.update(renderer, scene, camera);
 
@@ -54,18 +71,33 @@ export class DemoComponent implements OnInit {
 
   getBox(w, h, d) {
     let geometry = new THREE.BoxGeometry(w, h, d);
-    let material = new THREE.MeshBasicMaterial({ 
-      color: 0x00f0
+    let material = new THREE.MeshPhongMaterial({ 
+      color: 'rgb(120, 120, 120)'
     });
     let mesh = new THREE.Mesh(geometry, material);
     return mesh;
   }
 
+  getPointLight(color, intensity) {
+    let light = new THREE.PointLight(color, intensity);
+
+    return light;
+  }
+
   getPlane(size) {
     let geometry = new THREE.PlaneGeometry(size, size);
-    let material = new THREE.MeshBasicMaterial({ 
-      color: 0xfcba03,
+    let material = new THREE.MeshPhongMaterial({ 
+      color: 'rgb(120, 120, 120)',
       side: THREE.DoubleSide
+    });
+    let mesh = new THREE.Mesh(geometry, material);
+    return mesh;
+  }
+
+  getSphere(radius) {
+    let geometry = new THREE.SphereGeometry(radius, 24, 24);
+    let material = new THREE.MeshBasicMaterial({ 
+      color: '#ffffff'
     });
     let mesh = new THREE.Mesh(geometry, material);
     return mesh;
@@ -76,6 +108,16 @@ export class DemoComponent implements OnInit {
       scene,
       camera
     );
+
+    // let plane = scene.getObjectByName("plane-1");
+    // plane.rotation.y += 0.001;
+    // plane.rotation.z += 0.001;
+
+    // scene.traverse((child) => {
+    //   child.scale.x += 0.0001;
+    //   child.scale.y += 0.0001;
+    //   child.scale.z += 0.0001;
+    // })
 
     requestAnimationFrame(() => {
       this.update(renderer, scene, camera);
