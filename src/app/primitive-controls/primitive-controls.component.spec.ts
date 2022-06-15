c0cfcee60d904bca41d7ef17d3995747dfce4813
histlog <safejp1@gmail.com>
2022-06-15 21:30:30 +0530

    Updated project angular version

--- src/app/primitive-controls/primitive-controls.component.spec.ts
+++ src/app/primitive-controls/primitive-controls.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PrimitiveControlsComponent } from './primitive-controls.component';
 
@@ -6,7 +6,7 @@ describe('PrimitiveControlsComponent', () => {
   let component: PrimitiveControlsComponent;
   let fixture: ComponentFixture<PrimitiveControlsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PrimitiveControlsComponent ]
     })
